ef484f71d8c146bd9601715a9950958a36b009a9
Andrew Dales <andrew.dales@highgateschool.org.uk>
2022-10-12 18:34:13 +0100

    Added Bank object to bank_account.py

--- Object Oriented Programming/bank_account.py
+++ Object Oriented Programming/bank_account.py
@@ -8,10 +8,11 @@ import pyinputplus as pyip
 
 
 class BankAccount:
-    def __init__(self, account_number, password, balance=0):
+    def __init__(self, account_number, password, pin, balance=0):
         self.balance = balance
         self.account_number = account_number
         self.password_encoded = sha224(password.encode())
+        self.pin_encoded = sha224(pin.encode())
 
     def __str__(self):
         return f'BankAccount({self.account_number})'
@@ -39,6 +40,21 @@ class BankAccount:
 
     def check_password(self, password):
         return self.password_encoded.digest() == sha224(password.encode()).digest()
+
+
+class Bank:
+    next_account_number = 10_000
+
+    def __init__(self, bank_name):
+        self.bank_name = bank_name
+        self.accounts = {}
+
+    def add_account(self, ac_password, ac_pin):
+        Bank.next_account_number += 1
+        self.accounts.append(BankAccount(Bank.account_number, ac_password, ac_pin))
+
+    def find_account(self, ac_number):
+        accounts = [account for account in self.accounts]
 
 
 # Press the green button in the gutter to run the script.
